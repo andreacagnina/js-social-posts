b91eb32f97079ed5e7f2576adcbefca92fd03318
andreacagnina <amirkal@libero.it>
2024-07-12 18:30:50 +0200

    [feat] try to btn

--- main.js
+++ main.js
@@ -64,33 +64,33 @@ function CreatedPost() {
         container.innerHTML += `<div class="post">
             <div class="post__header">
                 <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src=${element.author.image} alt="${element.author.name}">                    
+                <div class="post-meta__icon">
+                <img class="profile-pic" src=${element.author.image} alt="${element.author.name}">                    
                     </div>
                     <div class="post-meta__data">
                         <div class="post-meta__author">${element.author.name}</div>
                         <div class="post-meta__time">${calcMonth(new Date(element.created))} mesi fa</div>
-                    </div>                    
-                </div>
-            </div>
-            <div class="post__text">${element.content}</div>
-            <div class="post__image">
-                <img src="${element.media}" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
+                        </div>                    
+                        </div>
+                        </div>
+                        <div class="post__text">${element.content}</div>
+                        <div class="post__image">
+                        <img src="${element.media}" alt="">
+                        </div>
+                        <div class="post__footer">
+                        <div class="likes js-likes">
+                        <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1" id="btn">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
                         </a>
-                    </div>
-                    <div class="likes__counter">
+                        </div>
+                        <div class="likes__counter">
                         Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
-                    </div>
-                </div> 
-            </div>            
-        </div>`
+                        </div>
+                        </div> 
+                        </div>            
+                        </div>`
     });
 }
 
@@ -111,6 +111,14 @@ function calcMonth(date) {
 }
 
 let container = document.getElementById('container');
-
-
 CreatedPost();
+
+
+
+let btn = document.getElementById('btn');
+document.getElementById("btn").addEventListener("click", function () {
+    btn.classList.add('color-red');
+
+});
+
+
